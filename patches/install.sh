d8329a32235676cbdc51b75b6107801b99e81125
Azizb750 <azizb750@gmail.com>
2018-08-18 12:01:36 +0300

    Update install.sh

--- patches/install.sh
+++ patches/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rootdirectory="$PWD"
-dirs="bionic frameworks/av system/core system/sepolicy"
+dirs="bionic frameworks/av system/core system/sepolicy system/"
 
 for dir in $dirs ; do
 	cd $rootdirectory
